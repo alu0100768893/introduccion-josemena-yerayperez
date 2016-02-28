0ea8a745d7c8fc5cd418b4e2606fa35dc2991a8a
Yeray Pérez Peraza <alu0100783612@ull.edu.es>
2016-02-28 19:52:27 +0000

    Solucionado aceptar números exponenciales

--- temperature.js
+++ temperature.js
@@ -3,31 +3,47 @@ function calculate() {
   var result;
   var temp = original.value;
   var regexp = /^\s*([-+]?\d+(?:\.\d+)?)\s*(e[-+]?\d+(?:\.\d+)?)?\s*([fc]|far|(fa)|(ce)|(cel)|(celsius)|(fahrenheit))\s*$/i;
-  
+
   var m = temp.match(regexp);
-  
-  if (m) {
+
+if (m) {
     var num = m[1];
-    if(m[3]){
+
+    if(m[2]){
+      console.log("soy exponencial");
+      
         var f = m[2];
         var type = m[3];
-    }else{var type = m[2];}
-    console.log("valor del f: " + f);
-    console.log("valor del type: " + type);
-    console.log("valor del número antes: " + num);
+        num= num.toString()+f.toString();
+        num = parseFloat(num);
+
+        if (type == 'c' || type == 'C') {
+          result = (num * 9/5)+32;
+          result = result.toFixed(2)+" Farenheit"
+        }
+        else {
+          result = (num - 32)*5/9;
+          result = result.toFixed(2)+" Celsius"
+        }
+        converted.innerHTML = result;
+
+    }else{
+      console.log("no soy exponencial");
+
     num = parseFloat(num);
-    console.log("valor del número después: " + num);
+
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = result.toFixed(1)+" Farenheit"
+      result = result.toFixed(2)+" Farenheit"
     }
     else {
       result = (num - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
+      result = result.toFixed(2)+" Celsius"
     }
     converted.innerHTML = result;
   }
-  else {
+  }else {
     converted.innerHTML = "¡ERROR! Pruebe algo como '-4.2C' o '37f'";
   }
-}
+
+}//cierre funcion
